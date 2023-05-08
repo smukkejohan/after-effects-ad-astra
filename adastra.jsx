9807dd6079134eb05e9601425535786440277258
ole kristensen <k-denfrie-ok@tv2.dk>
2023-05-08 11:26:44 +0200

    only include adastra once

--- adastra.jsx
+++ adastra.jsx
@@ -7,23 +7,26 @@
 
 // todo set variable to check if it has been included, so it only ever gets included once 
 
-var AIRLOOKFRAMEWORK_PATH = new File($.fileName).path;
-var OS = $.os;
+if(typeof ADASTRA_PATH === "undefined"){
 
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/prototype.jsx");
-
-// The order of these includes matter!
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/debug.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/log.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/fonts.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/utils.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/utils_keys.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/vectorMath.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/3d.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/paragraph.jsx"); // TODO: Update with new boxText methods
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/paragraph2D.jsx");
-$.evalFile(AIRLOOKFRAMEWORK_PATH + "/library/json2.jsx");
-
-// Define Global variables
-// TODO: make library not dependent on theese
-var mainComp, projectName, project, path, projectVersion;
+    var ADASTRA_PATH = new File($.fileName).parent.fsName;
+    var OS = $.os;
+    
+    $.evalFile(ADASTRA_PATH + "/library/prototype.jsx");
+    
+    // The order of these includes matter!
+    $.evalFile(ADASTRA_PATH + "/library/debug.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/log.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/fonts.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/utils.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/utils_keys.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/vectorMath.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/3d.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/paragraph.jsx"); // TODO: Update with new boxText methods
+    $.evalFile(ADASTRA_PATH + "/library/paragraph2D.jsx");
+    $.evalFile(ADASTRA_PATH + "/library/json2.jsx");
+    
+    // Define Global variables
+    // TODO: make library not dependent on theese
+    var mainComp, projectName, project, path, projectVersion;
+}
